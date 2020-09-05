f43ebf901fd0f811700b099287510d5f6c529bd4
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-09-06 03:16:56 +0900

    add bodyparser and api

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencode 로 된 데이터를 분석해서 가져올 수 있음
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 //mongodb+srv://wonhopark89:<password>@boilerplate-react-node.tbh99.mongodb.net/<dbname>?retryWrites=true&w=majority
 const mongoose = require('mongoose');
@@ -19,6 +27,18 @@ mongoose
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/register', (req, res) => {
+  // 회원가입할때 필요한 정보를 client 에서 가져온다
+  // 그것들은 데이터베이스에 넣어준다
+  const user = new User(req.body); // parser 가 있기 때문,
+  user.save((err, doc) => {
+    if (err) {
+      return res.json({ success: false, err }); // 실패했을 때 json 데이터로 응답
+    }
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
