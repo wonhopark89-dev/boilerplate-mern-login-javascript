78ee295298bc7d9c2e709976797253afe76e5435
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-09-07 01:28:17 +0900

    solve cors problem

---
+++ client/src/setupProxy.js
@@ -0,0 +1,14 @@
+/**
+ * https://create-react-app.dev/docs/proxying-api-requests-in-development/
+ */
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  );
+};
