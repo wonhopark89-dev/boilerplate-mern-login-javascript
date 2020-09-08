e1c86d4117fd21335d218a174387d447ec9de458
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-09-09 00:20:09 +0900

    add logout function

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
+  const onClickHandler = () => {
+    axios.get('/api/users/logout').then((response) => {
+      console.log(response.data);
+      if (response.data.success) {
+        props.history.push('/login');
+      } else {
+        alert('로그아웃 실패 !');
+      }
+    });
+  };
+
   return (
     <div
       style={{
@@ -12,6 +24,8 @@ function LandingPage() {
       }}
     >
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
